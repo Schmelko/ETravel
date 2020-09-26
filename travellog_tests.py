d0ebb514039dcb61c807816ab35599a3b565485c
Arnold Barna <arnold.barna@gmail.com>
2020-09-26 17:54:29 +0200

    Refactor tests

--- travellog_tests.py
+++ travellog_tests.py
@@ -9,14 +9,14 @@ class TestSolution(unittest.TestCase):
         with open('utasadat.txt') as f:
             lines = f.readlines()
         
-        self.mcard_log = TravelLog(lines)
+        self.travel_log = TravelLog(lines)
 
     def test_task1(self):
-        self.assertEqual(len(self.mcard_log.entries), 699)
+        self.assertEqual(len(self.travel_log.entries), 699)
 
     def test_task2(self):
         expected = 699
-        result = len(self.mcard_log.ticket_id_unique())
+        result = len(self.travel_log.ticket_id_unique())
         self.assertEqual(expected, result)        
 
 if __name__ == '__main__':
