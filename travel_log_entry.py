5a7478948d6a5c8411861f0333d0f2fa1dbec47b
Arnold Barna <arnold.barna@gmail.com>
2020-09-26 19:11:59 +0200

    Improve TravelLogEntry

--- travel_log_entry.py
+++ travel_log_entry.py
@@ -1,12 +1,18 @@
+from datetime import datetime
+
 class TravelLogEntry:
     '''One log entry.'''
 
     def __init__(self, line):
         raw = line.split()
         self.stop_id = int(raw[0])
-        self.get_on_datetime = raw[1]
+        self.get_on_datetime = datetime.strptime(raw[1], '%Y%m%d-%H%M')
         self.ticket_id = int(raw[2])
         self.ticket_type =  (raw[3])
-        self.ticket_expiration = raw[4]
-        
-  
+        if self.isPass():
+            self.no_of_ticket_left = int(raw[4])
+        else:
+            self.ticket_expiration = datetime.strptime(raw[4], '%Y%m%d')
+
+    def isPass(self):
+        return self.ticket_type == 'JGY'
