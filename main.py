c2a2c99881c6c99be775daaa932652da64b71f53
Arnold Barna <arnold.barna@gmail.com>
2020-09-28 19:34:08 +0200

    Add main.py formatted print

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
-from mcard_log import McardLog
+from travel_log import TravelLog
 
 with open('utasadat.txt') as f:
     lines = f.readlines()
-    mcard_log = McardLog(lines)
+    travel_log = TravelLog(lines)
 
-print(mcard_log)
+print(travel_log)
 
-print(len(mcard_log.ticket_id_unique()))
+print(len(travel_log.ticket_id_unique()))
+
+notifiables = travel_log.find_notifiables_of_coming_expiration(3)
+notifiables_content = tuple("{}\t{}".format(notifiable.ticket_id, notifiable.ticket_expiration.strftime('%Y-%m-%d')) for notifiable in notifiables)
+
+with open("figyelmeztetes.txt", "w") as f:
+    f.writelines('\n'.join(notifiables_content))
